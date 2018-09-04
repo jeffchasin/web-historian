b150256f0fb27718595ec8a96e268159b3398c97
Jeff Chasin <jeff@jeffchasin.com>
2018-09-03 20:06:22 -0400

    5 passing, 4 left

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,31 +5,66 @@ var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
-  if (req.method === 'GET' && req.url === '/') {
-    httpHelpers.serveAssets(res, 'homePage');
-  } else if (req.method === 'GET' && req.url) {
-    res.writeHead(200, exports.headers);
-    fs.readFile(archive.paths.archivedSites + '/' + req.url, 'utf8', (err, data) => {
-    // fs.readFile(archive.paths.archivedSites + '/' + archiveDomain + '/index.html', 'utf8', (err, data) => {
-      if (err) {
-        console.error('GET of req.url not in archive. err: ', err);
-        res.writeHead(404, httpHelpers.headers);
-        res.end('404 Error: Sorry, that page was not found.');
-      }
-      var temp = '';
-      temp += data;
-      // res.write(temp)
-      res.end(temp);
+
+  //GET REQUESTS
+
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      httpHelpers.serveAssets(res, 'homePage');
+    } else if (req.url === '/styles.css') {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+
+      fs.readFile(__dirname + '/public/styles.css', 'utf8', (err, data) => {
+        if (err) {
+          console.error('styles.css err: ', err);
+        }
+        var temp = '';
+        temp += data;
+        // res.write(temp);
+        res.end(temp);
+      });
+    } else {
+      res.writeHead(200, httpHelpers.headers);
+      fs.readFile(archive.paths.archivedSites + '/' + req.url, 'utf8', (err, data) => {
+        // fs.readFile(archive.paths.archivedSites + '/' + archiveDomain + '/index.html', 'utf8', (err, data) => {
+        if (err) {
+          console.error('GET of req.url not in archive. err: ', err);
+          res.writeHead(404, httpHelpers.headers);
+          res.end('404 Error: Sorry, that page was not found.');
+        }
+        var temp = '';
+        temp += data;
+        // res.write(temp)
+        res.end(temp);
+      });
+    }
+  }
+
+  //POST REQUESTS
+  if (req.method === 'POST') {
+    // check for the url they want
+    var siteUrl;
+    req.on('data', data => {
+      var input = '';
+      input += data;
+      input = input.split('=');
+      siteUrl = input[1];
+    }).on('end', () => {
+      fs.appendFile(archive.paths.list, siteUrl + '\n', 'utf8', (err) => {
+        if (err) {
+          console.error('appendFile err', err);
+        } else {
+          console.log('siteUrl: ', siteUrl);
+          console.log('apl: ', archive.paths.list);
+          // httpHelpers.serveAssets(res, siteUrl);
+        }
+      });
+      res.writeHead(302, { 'Content-Type': 'text/plain' });
+      res.end();
     });
+    // res.end(archive.paths.list);
   }
-  if (req.method === 'GET' && req.url === 'style.css') {
-    httpHelpers.serveAssets(res, 'css');
-  }
-
-
-  // res.end(archive.paths.list);
 };
-
 
 
 // var statusCode;
@@ -97,4 +132,3 @@ exports.handleRequest = function (req, res) {
 //   }
 
 //   // res.end(archive.paths.list);
-// };
