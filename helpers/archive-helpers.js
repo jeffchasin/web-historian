6d8a5d20858eb3a6fd339ea485b669170c132a2a
Jeff Chasin <jeff@jeffchasin.com>
2018-09-03 21:24:43 -0400

    9 tests passing

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -16,8 +17,8 @@ exports.paths = {
 };
 
 // Used for stubbing paths for tests, do not modify
-exports.initialize = function(pathsObj) {
-  _.each(pathsObj, function(path, type) {
+exports.initialize = function (pathsObj) {
+  _.each(pathsObj, function (path, type) {
     exports.paths[type] = path;
   });
 };
@@ -25,7 +26,7 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback) {
+exports.readListOfUrls = function (callback) {
   fs.readFile(this.paths.list, 'utf8', (err, data) => {
     if (err) {
       console.error('readListOfUrls err: ', err);
@@ -35,15 +36,15 @@ exports.readListOfUrls = function(callback) {
   });
 };
 
-exports.isUrlInList = function(url, callback) {
+exports.isUrlInList = function (url, callback) {
   // is url in the list?
-  this.readListOfUrls(function(atf) {
+  this.readListOfUrls(function (atf) {
     callback(atf.includes(url));
   });
 };
 
-exports.addUrlToList = function(url, callback) {
-  this.isUrlInList(url, function(included) {
+exports.addUrlToList = function (url, callback) {
+  this.isUrlInList(url, function (included) {
     if (included) {
       callback(included);
     } else {
@@ -57,9 +58,9 @@ exports.addUrlToList = function(url, callback) {
   });
 };
 
-exports.isUrlArchived = function(url, callback) {
+exports.isUrlArchived = function (url, callback) {
   var myFile = exports.paths.archivedSites + '/' + url;
-  fs.readFile(myFile, function(err, data) {
+  fs.readFile(myFile, function (err) {
     if (err) {
       callback(false);
     } else {
@@ -68,5 +69,17 @@ exports.isUrlArchived = function(url, callback) {
   });
 };
 
-exports.downloadUrls = function(urls) {
+exports.downloadUrls = function (urls) {
+  urls.forEach(function (url) {
+    request('http://' + url, function (error, response, body) {
+      // console.log('error:', error); // Print the error if one occurred
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      // console.log('body:', body); // Print the HTML for the Google homepage.
+      fs.writeFile(exports.paths.archivedSites + '/' + url, body, 'utf8', (err) => {
+        if (err) {
+          console.error('writeFile err: ', err);
+        }
+      });
+    });
+  });
 };
