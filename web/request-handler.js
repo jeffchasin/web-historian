f2c33ea302d153c6f074d77ad9c36a1c4a00f3d1
Jeff Chasin <jeff@jeffchasin.com>
2018-09-04 23:07:00 -0400

    htmlfetcher.js

--- web/request-handler.js
+++ web/request-handler.js
@@ -61,95 +61,24 @@ exports.handleRequest = function (req, res) {
               res.writeHead(404, httpHelpers.headers);
               res.end('404 Error: Sorry, that page was not found.');
             }
+            archive.addUrlToList(siteUrl, function(included) {
+              return included; // this return does nothing! Fake return.
+            });
             var temp = '';
             temp += data;
-            res.writeHead(200, httpHelpers.headers);
+            res.writeHead(302, httpHelpers.headers);
             res.end(temp);
           });
         } else {
           httpHelpers.serveAssets(res, 'loading');
+          // TODO: Broken test, still works in the app
           archive.addUrlToList(siteUrl, function(included) {
-            // TODO: YOU ARE HERE -------------------------------------- ******
-            return included; // this return does nothing! Fake return.
+            if (included) {
+              return siteUrl;
+            }
           });
         }
       });
     });
-
-    // fs.appendFile(archive.paths.list, siteUrl + '\n', 'utf8', (err) => {
-    //   if (err) {
-    //     console.error('appendFile err', err);
-    //   }
-    // });
-    // res.writeHead(302, { 'Content-Type': 'text/plain' });
-    // res.end();
   }
-  // res.end(archive.paths.list);
 };
-
-
-// var statusCode;
-
-// exports.handleRequest = function (req, res) {
-
-//   if (req.url === '/') {
-//     if (req.method.toUpperCase() === 'OPTIONS') {
-//       res.writeHead(
-//         '200',
-//         'No Content',
-//         httpHelpers.optionsHeaders
-//       );
-//       response.end();
-//     }
-
-
-//     if (req.method.toUpperCase() === 'GET') {
-//       // if we're here, they want the home page
-//       httpHelpers.serveAssets(res, 'homePage');
-//     }
-
-//     if (req.method.toUpperCase() === 'POST') {
-//       // check for the url they want
-//       var siteUrl;
-//       req.on('data', data => {
-//         var input = '';
-//         input += data;
-//         input = input.split('=');
-//         siteUrl = input[1];
-//       }).on('end', () => {
-//         // see if that's in our archived sites
-//         console.log('archive.isUrlArchived(siteUrl): ', archive.isUrlArchived(siteUrl));
-
-//         archive.isUrlArchived(siteUrl, function(fileExists) {
-//           console.log('fileExists: ', fileExists);
-//           if (fileExists) {
-//             // if archived, send back the right page
-//             httpHelpers.serveAssets(res, siteUrl);
-//           }
-//         });
-
-//         // if (archive.isUrlArchived(siteUrl )) {
-//         //   // if archived, send back the right page
-//         //   httpHelpers.serveAssets(res, siteUrl);
-//         // }
-//         // else {
-//         // if not archived, is it in the list?
-//         if (!archive.isUrlInList(siteUrl)) {
-//           //console.log('siteUrl: ', siteUrl);
-//           archive.addUrlToList(siteUrl);
-//         }
-//         // TODO: httpHelpers.serveAssets(res, 'loading');
-//         // respond with loading.html
-//       });
-//     }
-//   } else if (req.url === archive.paths.archivedSites + '/site-name/') {
-//     httpHelpers.serveAssets(res, 'homePage');
-//     // TODO: do stuff here
-//   } else if (req.url === '/styles.css') {
-//     httpHelpers.serveAssets(res, 'css');
-//   } else {
-//     res.writeHead(404, httpHelpers.headers);
-//     res.end('404 Error: Sorry, that page was not found.');
-//   }
-
-//   // res.end(archive.paths.list);
